3218b97fc582e29130f24a4c929c95047a67ad24
ilovezerocokeya <147008077a@gmail.com>
2024-06-18 19:38:11 +0900

    first commit

---
+++ src/pages/Diary.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+
+const Diary = () => {
+  const params = useParams();
+  console.log(params);
+  return <div>{params.id}번째 일기 입니다.</div>;
+};
+
+export default Diary;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { useSearchParams } from "react-router-dom";
+
+const Home = () => {
+  const [params, setParams] = useSearchParams();
+  console.log(params.get("value"));
+
+  return <div></div>;
+};
+
+export default Home;
