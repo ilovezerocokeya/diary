e23eb31d45433fdc9f7ac64761726f3545f86eb3
ilovezerocokeya <147008077a@gmail.com>
2024-06-18 20:17:53 +0900

    공통컴포넌트작업

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import Home from "./pages/Home";
 import New from "./pages/New";
 import Diary from "./pages/Diary";
-import { Link, Route, Routes, useNavigate } from "react-router-dom";
+import { Route, Routes, useNavigate } from "react-router-dom";
 import Notfound from "./pages/Notfound";
-import { getEmotionImage } from "./util/get-emotion-image";
+import Button from "./components/Button";
+import Header from "./components/Header";
 
 const App = () => {
   const nav = useNavigate();
@@ -12,20 +13,33 @@ const App = () => {
   };
   return (
     <>
-      <div>
-        <img src={getEmotionImage(1)} />
-        <img src={getEmotionImage(2)} />
-        <img src={getEmotionImage(3)} />
-        <img src={getEmotionImage(4)} />
-        <img src={getEmotionImage(5)} />
-      </div>
+      <Header
+        title={"Header"}
+        leftChild={<Button text={"Left"} />}
+        rightChild={<Button text={"Right"} />}
+      />
+      <Button
+        text={"123"}
+        type={"POSITIVE"}
+        onClick={() => {
+          console.log("123번 버튼 클릭!");
+        }}
+      />
+      <Button
+        text={"123"}
+        type={"NEGATIVE"}
+        onClick={() => {
+          console.log("123번 버튼 클릭!");
+        }}
+      />
+      <Button
+        text={"123"}
+        type={"DEFAULT"}
+        onClick={() => {
+          console.log("123번 버튼 클릭!");
+        }}
+      />
 
-      <div>
-        <Link to="/">Home</Link>
-        <Link to="/new">New</Link>
-        <Link to="/diary">Diary</Link>
-      </div>
-      <button onClick={onClickButton}>New 페이지로 이동</button>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/new" element={<New />} />
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,9 +3,8 @@ import { useSearchParams } from "react-router-dom";
 
 const Home = () => {
   const [params, setParams] = useSearchParams();
-  console.log(params.get("value"));
 
-  return <div></div>;
+  return <div>Home</div>;
 };
 
 export default Home;
